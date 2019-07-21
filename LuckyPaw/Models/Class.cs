3a428b99900e18ac8332df96979f8cfce5fbff0b
Evan J Luputra <eluputra@hotmail.com>
2019-07-21 00:23:06 -0700

    WebApp1b
    First Submision will develop more day by day

---
+++ LuckyPaw/Models/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuckyPaw.Models
+{
+    public class Class
+    {
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# LuckyPaw
+Evan Luputra
+eluputra@hotmail.com
+ASP.Net program for TCSS 431
